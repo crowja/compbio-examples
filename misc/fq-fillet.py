44038a259d11464ade76449a7e5b92f1b61acac7
John Crow <johna.crow@corteva.com>
2021-10-25 16:12:05 -0500

    Added option to specify end to trim.

--- misc/fq-fillet.py
+++ misc/fq-fillet.py
@@ -17,6 +17,13 @@ def get_options():
         default=0,
         help="Produce reads this long when possible, default (0) full length.",
     )
+    p.add_argument(
+        "-e",
+        "--end",
+        choices={"L", "R", "LR"},
+        default="LR",
+        help="Trim from left, right, or both symmetrically",
+    )
     return p.parse_args()
 
 
@@ -30,12 +37,19 @@ if __name__ == "__main__":
         start = 0
         end = seq_len
         if targ_len > 0 and targ_len < seq_len:
-            d = int((seq_len - targ_len) / 2)
-            start = d
-            end = start + targ_len
+            if args.end == "LR":
+                d = int((seq_len - targ_len) / 2)
+                start = d
+                end = start + targ_len
+            elif args.end == "R":
+                start = 0
+                end = targ_len
+            else:  # args.end == "L"
+                start = seq_len - targ_len
+                end = seq_len
         ##print(f"Sequence length is {seq_len}. The fillet is {start} to {end}.")
         try:
             print(r[start:end].format("fastq"))
         except:
-            print(f"fq-fillet.bash failed on sequence {r.id}, exiting", file=sys.stderr) 
+            print(f"fq-fillet.bash failed on sequence {r.id}, exiting", file=sys.stderr)
             exit(1)
